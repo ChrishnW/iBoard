a73c111eefdacc0aeeb444da6c21d24eaf4d4fa8
s3rb0g <raniegobres@gmail.com>
2025-03-14 14:22:51 +0800

    refactor: update account.php to add account form and improve dashboard visibility

--- pages/account.php
+++ pages/account.php
@@ -2,7 +2,7 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-  <div id="department_dashboard" class="department_dashboard" style="display: block;">
+  <div id="department_dashboard" class="department_dashboard" style="display: none;">
 
     <div class="card shadow mb-4">
       <div class="card-header py-3.5">
@@ -27,7 +27,38 @@
         </div>
       </div>
     </div>
-  </div>
+</div>
+
+<div id="add_account" class="add_account" style="display: block;">
+    <h2>Add Account</h2>
+
+    <form action="admin.php" method="post">
+        <label for="acc_name">Username <span style="color: red;">*</span></label>
+        <input type="text" name="acc_name" id="acc_name" placeholder="SDRB" required><br><br>
+
+        <label for="acc_password">Password <span style="color: red;">*</span></label>
+        <input type="password" name="acc_password" id="acc_password" placeholder="*******" required><br><br>
+
+        <label for="acc_department_code">Department <span style="color: red;">*</span></label>
+        <select name="acc_department_code" id="acc_department_avail" required>
+            <option value="" hidden></option>
+
+        </select>
+
+        <label for="acc_status">Status <span style="color: red;">*</span></label>
+        <select name="acc_status" id="acc_status" required>
+            <option value="" hidden></option>
+            <option value="1">Active</option>
+            <option value="0">Inactive</option>
+        </select>
+
+        <br><br>
+        <input type="submit" name="add_account" value="Add Account" class="submit">
+        <input type="reset" name="reset" value="Cancel" id="cancel_account">
+
+    </form>
+
+</div>
 
 
 </div>
