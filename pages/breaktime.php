22c428b983d6b6399655d1ec1160c378fcf1ffd0
s3rb0g <raniegobres27@gmail.com>
2025-03-15 07:19:22 +0800

    refactor: standardize indentation for breaktime input fields in breaktime.php

--- pages/breaktime.php
+++ pages/breaktime.php
@@ -49,37 +49,37 @@
       <input type="text" name="break_code" id="break_code" placeholder="101" required><br><br>
 
       <div id="breaktime">
-          <label for="break_start_am">Breaktime Start (AM)</label>
-          <input type="time" name="break_start_am" id="break_start_am" placeholder="00:00" required><br><br>
+        <label for="break_start_am">Breaktime Start (AM)</label>
+        <input type="time" name="break_start_am" id="break_start_am" placeholder="00:00" required><br><br>
 
-          <label for="break_end_am">Breaktime End (AM)</label>
-          <input type="time" name="break_end_am" id="break_end_am" placeholder="00:00" required><br><br>
+        <label for="break_end_am">Breaktime End (AM)</label>
+        <input type="time" name="break_end_am" id="break_end_am" placeholder="00:00" required><br><br>
 
-          <label for="break_start_lunch">Breaktime Start (Lunch)</label>
-          <input type="time" name="break_start_lunch" id="break_start_lunch" placeholder="00:00" required><br><br>
+        <label for="break_start_lunch">Breaktime Start (Lunch)</label>
+        <input type="time" name="break_start_lunch" id="break_start_lunch" placeholder="00:00" required><br><br>
 
-          <label for="break_end_lunch">Breaktime End (Lunch)</label>
-          <input type="time" name="break_end_lunch" id="break_end_lunch" placeholder="00:00" required><br><br>
+        <label for="break_end_lunch">Breaktime End (Lunch)</label>
+        <input type="time" name="break_end_lunch" id="break_end_lunch" placeholder="00:00" required><br><br>
 
-          <label for="break_start_pm">Breaktime Start (PM)</label>
-          <input type="time" name="break_start_pm" id="break_start_pm" placeholder="00:00" required><br><br>
+        <label for="break_start_pm">Breaktime Start (PM)</label>
+        <input type="time" name="break_start_pm" id="break_start_pm" placeholder="00:00" required><br><br>
 
-          <label for="break_end_pm">Breaktime End (PM)</label>
-          <input type="time" name="break_end_pm" id="break_end_pm" placeholder="00:00" required><br><br>
+        <label for="break_end_pm">Breaktime End (PM)</label>
+        <input type="time" name="break_end_pm" id="break_end_pm" placeholder="00:00" required><br><br>
 
-          <label for="break_start_ot">Breaktime Start (OT)</label>
-          <input type="time" name="break_start_ot" id="break_start_ot" placeholder="00:00" required><br><br>
+        <label for="break_start_ot">Breaktime Start (OT)</label>
+        <input type="time" name="break_start_ot" id="break_start_ot" placeholder="00:00" required><br><br>
 
-          <label for="break_end_ot">Breaktime End (OT)</label>
-          <input type="time" name="break_end_ot" id="break_end_ot" placeholder="00:00" required><br><br>
+        <label for="break_end_ot">Breaktime End (OT)</label>
+        <input type="time" name="break_end_ot" id="break_end_ot" placeholder="00:00" required><br><br>
       </div>
       
 
       <label for="break_status">Status</label>
       <select name="break_status" id="break_status" required>
-          <option value="" hidden></option>
-          <option value="1">Active</option>
-          <option value="0">Inactive</option>
+        <option value="" hidden></option>
+        <option value="1">Active</option>
+        <option value="0">Inactive</option>
       </select> 
 
       <br><br>
