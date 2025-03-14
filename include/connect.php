287565f2ba48b9523db8baae8cf217d43afbf48e
ChrishnW <christianjohn.lopez@cvsu.edu.ph>
2025-03-14 10:32:55 +0800

    change connect

--- include/connect.php
+++ include/connect.php
@@ -1,8 +1,8 @@
 <?php
-$db_host      = 'localhost';
+$db_host      = '192.168.5.220';
 $db_user      = 'root';
-$db_database  = 'ict_asset_inventory';
-$db_pass      = '';
+$db_database  = 'iboard';
+$db_pass      = 'p@55w0rd$$$';
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_database);
 if ($conn->connect_error) {
