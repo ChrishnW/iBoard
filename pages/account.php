fea1c005af0029e3894772c501dcee403f930e00
s3rb0g <raniegobres27@gmail.com>
2025-03-15 09:44:22 +0800

    feat: add functions to retrieve department names and display account list with status

--- pages/account.php
+++ pages/account.php
@@ -1,4 +1,49 @@
-<?php include '../include/header.php'; ?>
+<?php include '../include/header.php'; 
+
+  function getDepartmentName_string($dept_code){
+    
+    global $conn;
+
+    $sql_command = "SELECT dept_name FROM tbl_department WHERE dept_code = '$dept_code'";
+    $result = mysqli_query($conn, $sql_command);
+
+    if(mysqli_num_rows($result) > 0){
+      $department = mysqli_fetch_assoc($result);
+      return $department["dept_name"];
+    }
+    else{
+      return "N/A";
+    }
+  }
+
+  function getAllDepartment(){
+
+    global $conn;
+
+    $sql_command = "SELECT * FROM tbl_department WHERE status = '1'";
+    $result = mysqli_query($conn, $sql_command);
+
+    if(mysqli_num_rows($result) > 0){
+      while($department = mysqli_fetch_assoc($result)){
+
+        $dept_name = $department["dept_name"];
+        $dept_code = $department["dept_code"];
+
+        echo '<script> document.addEventListener("DOMContentLoaded", function () {
+            const table = `
+            <option value="' . $dept_code . '">' . $dept_name . '</option>`;
+            
+            document.querySelector("#edit_acc_code").insertAdjacentHTML("beforeend", table);
+        });</script>';
+
+      }
+    }
+  }
+
+
+?>
+
+
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
@@ -81,7 +126,73 @@
 <!-- /.container-fluid -->
 <?php include '../include/footer.php'; 
 
+  // Display Account List----------------------------------------------------------------------------
 
+  $sql_command = "SELECT * FROM tbl_accounts WHERE access = '2'";
+  $result = mysqli_query($conn, $sql_command);
+
+  if(mysqli_num_rows($result) > 0){
+      while($account = mysqli_fetch_assoc($result)){
+
+        $acc_id = $account["id"];
+        $username = $account["username"];
+        $dept_code = $account["dept_code"];
+        $status = $account["status"];
+        $status_word = "";
+
+        $dept_string = getDepartmentName_string($dept_code);
+
+        if($status == "1"){
+            $status_word = "Active";
+        }
+        else{
+            $status_word = "Inactive";
+        }
+
+        echo '<script> document.addEventListener("DOMContentLoaded", function () {
+            const table = `
+            <tr>
+                <td>' . $acc_id . '</td>
+                <td>' . $username . '</td>
+                <td>' . $dept_string . '</td>
+                <td>' . $status_word . '</td>
+                <td>
+                    <form action="account.php" method="post" class="form_table">
+                        <input type="hidden" name="id_account" value=' . $acc_id . '>
+
+                        <input type="submit" id="edit_account" class="edit" value="Edit" name="edit_account">
+                        <input type="submit" id="delete_account" class="delete" value="Delete" name="delete_account">
+
+                    </form>
+                </td>
+            </tr>`;
+            
+            document.querySelector("#dataTable").insertAdjacentHTML("beforeend", table);
+        });</script>';
+
+      }
+  }
+
+  // Display All Department in Add Account --------------------------------------------------------------------------
+
+  $sql_command = "SELECT * FROM tbl_department WHERE status = '1'";
+  $result = mysqli_query($conn, $sql_command);
+
+  if(mysqli_num_rows($result) > 0){
+    while($department = mysqli_fetch_assoc($result)){
+
+      $dept_name = $department["dept_name"];
+      $dept_code = $department["dept_code"];
+
+      echo '<script> document.addEventListener("DOMContentLoaded", function () {
+        const table = `
+        <option value="' . $dept_code . '">' . $dept_name . '</option>`;
+        
+        document.querySelector("#acc_department_avail").insertAdjacentHTML("beforeend", table);
+      });</script>';
+
+    }
+  }
 
 ?>
 
@@ -92,12 +203,17 @@
     const btn_add_department = document.getElementById('btn_add_department');
     const department_dashboard = document.getElementById('department_dashboard');
     const add_account = document.getElementById('add_account');
+    const cancel_account = document.getElementById('cancel_account');
 
     btn_add_department.addEventListener("click", function(){
       department_dashboard.style.display = 'none';
       add_account.style.display = 'block';
     });
     
+    cancel_account.addEventListener("click", function(){
+      department_dashboard.style.display = 'block';
+      add_account.style.display = 'none';
+    });
 
   });
 
