7d4981a3b4e5a734c9fc4ee3c5079a05f484cf08
John Neil <johnmontiano8@gmail.com>
2025-03-17 07:46:56 +0800

    style: enhance layout and styling of monitor page for improved user experience

--- pages/monitor.php
+++ pages/monitor.php
@@ -1,41 +1,42 @@
-<?php include '../include/header.php'; ?>
-<!-- Begin Page Content -->
-<div class="container-fluid">
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <?php include '../include/link.php'; ?>
+    <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="../vendor/snapappointments/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
+    
 </head>
-<body>
+<body class="container-fluid p-4" style="background-color: #add8e6;">
     <div>
-        <div class="d-flex align-items-center">
+        <div class="d-flex align-items-center px-3">
             <img src="../assets/img/logo.png" alt="logo.png" class="img-fluid mr-3 border" style="width: 50px;">
             <span class="h4 font-weight-bold mb-0">SDRP LINE</span>
             
-            <div class="ml-auto text-right">
+            <div class="ml-auto text-right mr-5">
                 <span class="h1 font-weight-bold mb-2 text-dark">RUN</span> <br>
                 <span class="h5 font-weight-bold mb-0 text-danger">00:00:00</span>
             </div>
         </div>
 
-        <div id="details" class="d-flex align-items-start mt-3">
+        <div id="details" class="d-flex align-items-start mt-3 px-5 mr-4">
             <img src="../assets/img/pexels-pixabay-434337.jpeg" alt="line" class="img-fluid mr-3 border" style="width: 350px;">
-            <div>
-                <span class="h5 font-weight-bold mb-2 text-danger text-decoration-underline">Information</span> <br>
-                <span class="h5 font-weight-bold mb-2">SDRB-200/260</span> <br>
-                <span class="h5 font-weight-bold mb-2 text-danger text-decoration-underline">Leader</span> <br>
-                <span class="h5 font-weight-bold mb-2">Juan Dela Cruz</span> <br>
+            <div class="d-flex flex-column pl-5 fs-1">
+            <span class="h3 text-danger text-decoration-underline">Information</span> 
+            <span class="h3 ">SDRB-200/260</span> 
+            <span class="h3 text-danger text-decoration-underline">Leader</span> 
+            <span class="h3 ">Juan Dela Cruz</span> 
             </div>
-            <div class="ml-auto">
-                <img src="../assets/img/undraw_profile.svg" alt="" class="img-fluid border" style="width: 80px;">
-            </div>
+            <div class="ml-auto align-self-end">
+            <img src="../assets/img/undraw_profile.svg" alt="" class="img-fluid border" style="width: 80px;">
+            </div>  
         </div>
 
-        <div class="card-body">
-            <div class="table-responsive">
+        <div class="card-body mt-3">
+            <div class="table-responsive bg-white">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead class="bg-primary text-white text-center">
                         <tr>
@@ -45,7 +46,7 @@
                             <th class="align-text-top">BALANCE</th>
                         </tr>
                     </thead>
-                    <tbody class="text-center">
+                    <tbody class="bg-white text-center">
                         <tr>
                             <td>100</td>
                             <td>7</td>
@@ -54,7 +55,6 @@
                                 <input type="submit" name="minus" value="-" class="btn btn-primary btn-sm">
                                 <input type="submit" name="plus" value="+" class="btn btn-primary btn-sm">
                             </div>
-                            </td>
                             </td>
                             <td class="h5 font-weight-bold mb-2 text-danger">-7</td>        
                         </tr>
@@ -65,7 +65,3 @@
     </div>
 </body>
 </html>
-
-</div>
-<!-- /.container-fluid -->
-<?php include '../include/footer.php'; ?>
