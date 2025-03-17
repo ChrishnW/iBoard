5bda92c698b220fdee3efe26d1dd03f6552e86f1
John Neil <johnmontiano8@gmail.com>
2025-03-17 10:25:39 +0800

    ;.|

--- pages/monitor.php
+++ pages/monitor.php
@@ -2,35 +2,101 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>iBoard | <?php echo ucfirst(basename($_SERVER['PHP_SELF'], '.php')); ?></title>
+    <title>iBoard | <?php echo ucfirst(basename($_SERVER['PHP_SELF'], '.php')); ?></title>
 
-  <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/logo.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/logo.png">
 
-    <?php include '../include/link.php'; ?>
-    <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <link href="../vendor/snapappointments/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
-    
+        <?php include '../include/link.php'; ?>
+        <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+        <link href="../vendor/snapappointments/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
+        
 </head>
 <body class="container-fluid p-4" style="background-color: #add8e6;">
-    <div>
+
+        <!-- EDIT MONITOR -->
+        <div id="edit_monitor" class="edit_monitor" style="display: block;">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3.5 pt-4">
+                    <h2 class="float-left">Edit Details</h2>        
+                </div>
+                <div class="card-body shadow-sm m-4 p-4">
+                    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" enctype="multipart/form-data" style="width: 100%;">
+                        <div class="row">
+                            <!-- Column 1: Line Details and Person -->
+                            <div class="col-md-6">
+                                <!-- Line Details -->
+                                <div class="mb-3">
+                                    <label for="dept_name" class="form-label">Line Name <span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" name="dept_name" id="dept_name" placeholder="SDRB LINE" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="dept_code" class="form-label">Line Description<span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" name="dept_code" id="dept_code" placeholder="101" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="line_image" class="form-label">Line Image<span class="text-danger">*</span></label>
+                                    <input type="file" class="form-control" name="line_image" id="line_image" required>
+                                </div>
+                                
+                                  <!-- Column 2: Table -->
+                                 <div class="mb-3">
+                                    <label for="daily_target" class="form-label">Daily Target<span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" name="daily_target" id="daily_target" placeholder="100" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="target_now" class="form-label">Target Now<span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" name="target_now" id="target_now" placeholder="7" required> 
+                                </div>  
+
+                            </div>
+                          
+                            <div class="col-md-6">
+                                <!-- Line Person -->
+                                <div class="mb-3">
+                                    <label for="line_leader" class="form-label">Line Leader<span class="text-danger">*</span></label>
+                                    <input type="text" class="form-control" name="line_leader" id="line_leader" placeholder="Juan Dela Cruz" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="line_leader_image" class="form-label">Line Leader Image<span class="text-danger">*</span></label>
+                                    <input type="file" class="form-control" name="line_leader_image" id="line_leader_image" required>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <br>
+                        <div class="d-flex justify-content-left">
+                            <input type="submit" name="add_department_submit" class="btn btn-primary pr-3" value="Edit">
+                            <input type="reset" name="reset" class="btn btn-secondary ml-2" value="Cancel" id="cancel_department">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    <div style="display: none;">
         <!-- Header Section -->
         <div class="d-flex align-items-center px-3">
             <img src="../assets/img/logo.png" alt="logo.png" class="img-fluid mr-3 border" style="width: 100px;">
-            <span class="h2 font-weight-bold mb-0 text-primary">SDRP LINE</span>
+            <span class="h2 font-weight-bold mb-0 text-primary">SDRB LINE</span>
             
             <div class="ml-auto text-right mr-5">
                 <button class="display-4 font-weight-bold mb-2 mr-3 text-dark" style="background-color: transparent; border: none;">RUN</button> <br>
                 <span class="h3 font-weight-bold mb-0 text-danger">00:00:00:00</span>
             </div>
 
-            <div id="settings" class="">
-                <button class="fa fa-cog fa-2x" aria-hidden="true" style="background-color: transparent; border: none;"></button>
+            <div id="settings" class="dropdown">
+                <button class="fa fa-cog fa-2x" aria-hidden="true" style="background-color: transparent; border: none;" data-toggle="dropdown"></button>
+                <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="#">Settings</a>
+                        <a class="dropdown-item" href="#">Logout</a>
+                </div>
             </div>
+
         </div>
 
         <!-- Details Section -->
@@ -41,7 +107,7 @@
                 <span class="h2 text-danger"><u>Information</u></span> 
                 <span class="h2 text-dark">SDRB-200/260</span> 
                 <span class="h2 text-danger"><u>Leader</u></span> 
-                <span class="h2 text-dark">Lee Serrano</span> 
+                <span class="h2 text-dark">Juan Dela Cruz</span> 
             </div>
 
             <div class="ml-auto align-self-end">
@@ -61,6 +127,7 @@
                             <th class="align-text-top">BALANCE</th>
                         </tr>
                     </thead>
+
                     <tbody class="bg-white text-center text-dark h4">
                         <tr>
                             <td class="font-weight-bolder" style="font-size: 50px;">100</td>
@@ -79,36 +146,33 @@
             </div>
         </div>
     </div>
+
+        <!-- Include jQuery and Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+
+        <script>
+                function add() {
+                        var actual = document.getElementById('actual_count').innerHTML;
+                        var target = document.getElementById('target_count').innerHTML;
+                        
+                        var new_actual = parseInt(actual) + 1;
+                        var new_balance = parseInt(target) - new_actual;
+
+                        document.getElementById('actual_count').innerHTML = new_actual;
+                        document.getElementById('balance_count').innerHTML = new_balance;
+                }
+
+                function minus() {
+                        var actual = document.getElementById('actual_count').innerHTML;
+                        var target = document.getElementById('target_count').innerHTML;
+                        
+                        var new_actual = parseInt(actual) - 1;
+                        var new_balance = parseInt(target) - new_actual;
+
+                        document.getElementById('actual_count').innerHTML = new_actual;
+                        document.getElementById('balance_count').innerHTML = new_balance;
+                }
+        </script>
 </body>
 </html>
-
-<?php
-
-?>
-
-<script>
-    function add() {
-
-        var actual = document.getElementById('actual_count').innerHTML;
-        var target = document.getElementById('target_count').innerHTML;
-        
-        var new_actual = parseInt(actual) + 1;
-        var new_balance = parseInt(target) - new_actual;
-
-        document.getElementById('actual_count').innerHTML = new_actual;
-        document.getElementById('balance_count').innerHTML = new_balance;
-
-    }
-
-    function minus() {
-
-        var actual = document.getElementById('actual_count').innerHTML;
-        var target = document.getElementById('target_count').innerHTML;
-        
-        var new_actual = parseInt(actual) - 1;
-        var new_balance = parseInt(target) - new_actual;
-
-        document.getElementById('actual_count').innerHTML = new_actual;
-        document.getElementById('balance_count').innerHTML = new_balance;
-    }
-</script>
