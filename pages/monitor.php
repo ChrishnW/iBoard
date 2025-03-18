c88e2a39dccb069a7186ca5a1be2c13de4f878a4
John Neil <johnmontiano8@gmail.com>
2025-03-18 13:21:32 +0800

    fix: remove unnecessary empty line in monitor.php

--- pages/monitor.php
+++ pages/monitor.php
@@ -59,7 +59,7 @@
                             <td>1</td>
                             <td class="text-danger">-2</td>
                         </tbody>
-        
+
                         <tbody class="text-black text-center">
                             <td class="font-weight-bold">Test</td>
                             <td class="font-weight-bold">Test 1</td>
