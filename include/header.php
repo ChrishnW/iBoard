13503e3a5ad42515e5171d284d309ad2d28a104e
John Neil <johnmontiano8@gmail.com>
2025-03-18 12:50:15 +0800

    fix: update logout button text and icon, and remove unnecessary empty tbody

--- include/header.php
+++ include/header.php
@@ -88,7 +88,7 @@
                 <img class="img-profile rounded-circle" src="../assets/img/user.png">
               </a>
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="../include/logout.php">
+                <a class="dropdown-item" href="/include/logout.php" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                   Logout
                 </a>
--- include/logout.php
+++ include/logout.php
@@ -3,5 +3,5 @@ session_start(); // Start the session
 session_unset(); // Unset all session variables
 session_destroy(); // Destroy the session
 
-header("Location: ../index.php"); // Redirect to the login page
+header("Location: /index.php"); // Redirect to the root index.php
 exit(); // Ensure no further code is executed
