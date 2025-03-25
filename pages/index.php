5b957364e651cf3873de9e9fa007775d06dc941a
s3rb0g <raniegobres27@gmail.com>
2025-03-25 12:01:47 +0800

    .

--- pages/index.php
+++ pages/index.php
@@ -3,7 +3,7 @@
 
   if(isset($_SESSION['tigger'])){
     
-    //unset($_SESSION['tigger']);
+    unset($_SESSION['tigger']);
 
     echo "<script>
       document.addEventListener('DOMContentLoaded', function () {
