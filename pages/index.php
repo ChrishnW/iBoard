7cf964b7fcbd7b29ad39b5fb1479773c88ada851
s3rb0g <raniegobres27@gmail.com>
2025-03-25 10:12:56 +0800

    Add new monitoring page and enhance index layout: implement submit button functionality and style updates

--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,27 @@
-<?php include '../include/header.php'; ?>
+<?php include '../include/header.php'; 
+
+
+  if(isset($_POST['submit'])){
+    echo "<script>
+      alert('Submit button clicked');
+    </script>";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
   <!-- Begin Page Content -->
-  <div class="container-fluid">
+  <div class="container-fluid" id="display_department" style="display: block;">
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
@@ -14,6 +35,76 @@
     </div>
 
   </div>
+
+  <div class="container-fluid" id="monitor_department" style="display: none;">
+
+    <div class="card shadow my-4">
+        <div class="card-header py-3.5 pt-4 align-items-center ">
+            <img src="../assets/img/logo.png" alt="logo.png" class="img-fluid mr-2 border" style="width: 55px;">
+            <h2 class="d-inline-block align-middle pt-2 text-primary font-weight-bold "><u>GPI Production Status</u></h2>
+            <a class="btn btn-danger float-right mt-2" href="index.php">
+                <i class="fas fa-sign-out-alt"></i> Exit
+            </a>
+            <div class="clearfix"></div>
+        </div>
+
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered table-sm table-striped" id="dataTable" width="100%" cellspacing="0">
+                    <thead class="bg-primary text-white text-center">
+                    <tr>
+                        <th rowspan="2" class="text-center align-middle">Model</th>
+                        <th rowspan="2" class="text-center align-middle">Unit</th>
+                        <th rowspan="2" class="text-center align-middle">Status</th>
+                        <th colspan="2" class="text-center align-middle">TARGET</th>
+                        <th rowspan="2" class="text-center align-middle">Actual</th>
+                        <th rowspan="2" class="text-center align-middle">Balance</th>
+                    </tr>
+                    <tr>
+                        <th class="text-center align-middle">(Day)</th>
+                        <th class="text-center align-middle">(Now)</th>
+                    </tr>
+                    </thead>
+
+                    <tbody class="text-black text-center" id="insert_here">
+                        
+                    </tbody>
+                    
+                </table>
+            </div>
+
+            <div class="d-flex justify-content-end">
+                <nav aria-label="...">
+                    <ul class="pagination">
+                        <li class="page-item disabled">
+                            <a class="page-link" href="#" tabindex="-1">Previous</a>
+                        </li>
+
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        
+                        <li class="page-item">
+                            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                        </li>
+
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        
+                        <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+  </div>
+
+
+
+
+
+
+
 <!-- /.container-fluid -->
 <?php include '../include/footer.php'; 
 
@@ -60,7 +151,10 @@
                                 <div class=\"ml-5\"><h3>$number</h3></div>
                               </div>
                           </div>
-                          <div><a href=\"\">more info -></a></div>
+                          <form action=\"index.php\" method=\"post\">
+                              <input type=\"hidden\" id=\"totalPages\" value=\"1\">
+                              <input type=\"submit\" name=\"submit\" value=\"More Info ->\" id=\"btn_submit1\">
+                          </form>
                       </div>
                   </div>
               </div>
@@ -76,12 +170,6 @@
 
     }
   }
-
-
-
-
-
-
 
 
 
---
+++ pages/page.php
@@ -0,0 +1,9 @@
+<?php include '../include/header.php'; ?>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+  <!-- Body Contents -->
+
+</div>
+<!-- /.container-fluid -->
+<?php include '../include/footer.php'; ?>
