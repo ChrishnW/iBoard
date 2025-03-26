cf1f793e83a8bb11551eb49b23caa4250d7af505
s3rb0g <raniegobres27@gmail.com>
2025-03-26 08:58:25 +0800

    Remove commented-out code for account, breaktime, and department listings

--- pages/account.php
+++ pages/account.php
@@ -559,53 +559,6 @@
 <!-- /.container-fluid -->
 <?php include '../include/footer.php'; 
 
-  // Display Account List----------------------------------------------------------------------------
-
-  // $sql_command = "SELECT * FROM tbl_accounts WHERE access = '2'";
-  // $result = mysqli_query($conn, $sql_command);
-
-  // if(mysqli_num_rows($result) > 0){
-  //     while($account = mysqli_fetch_assoc($result)){
-
-  //       $acc_id = $account["id"];
-  //       $username = $account["username"];
-  //       $dept_code = $account["dept_code"];
-  //       $status = $account["status"];
-  //       $status_word = "";
-
-  //       $dept_string = getDepartmentName_string($dept_code);
-
-  //       if($status == "1"){
-  //           $status_word = "Active";
-  //       }
-  //       else{
-  //           $status_word = "Inactive";
-  //       }
-
-  //       echo '<script> document.addEventListener("DOMContentLoaded", function () {
-  //           const table = `
-  //           <tr>
-  //               <td>' . $acc_id . '</td>
-  //               <td>' . $username . '</td>
-  //               <td>' . $dept_string . '</td>
-  //               <td>' . $status_word . '</td>
-  //               <td>
-  //                   <form action="account.php" method="post" class="form_table">
-  //                       <input type="hidden" name="id_account" value=' . $acc_id . '>
-
-  //                       <input type="submit" class="edit btn btn-primary ml-2" value="Edit" name="edit_account">
-  //                       <input type="submit" class="delete btn btn-danger" value="Delete" name="delete_account">
-
-  //                   </form>
-  //               </td>
-  //           </tr>`;
-            
-  //           document.querySelector("#dataTable").insertAdjacentHTML("beforeend", table);
-  //       });</script>';
-
-  //     }
-  // }
-
   // Display All Department in Add Account --------------------------------------------------------------------------
 
   $sql_command = "SELECT * FROM tbl_department WHERE status = '1'";
