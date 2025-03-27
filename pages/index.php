86076c3996f0da3cef405da5a18a338bb3916a80
John Neil <johnmontiano8@gmail.com>
2025-03-27 14:24:35 +0800

    (╯‵□′)╯︵┻━┻

--- pages/index.php
+++ pages/index.php
@@ -189,29 +189,27 @@
         document.addEventListener('DOMContentLoaded', function () {
 
           const table = `
-            <div class=\"col-lg-4 col-md-5 col-sm-10 mb-4\">
-                <div class=\"card shadow h-100\" style=\"border-radius: 15px; border-left: 5px solid #4e73df;\">
-                    <div class=\"card-body\">
-                        <div class=\"row justify-content-center\">
-                            <div class=\"text-center ml-2 mr-1\">
-                                <form action=\"index.php\" method=\"post\" class=\"d-flex flex-column align-items-center py-1\" style=\"gap: 5px; line-height: .85;\">
-                                    <input type=\"hidden\" name=\"depart_code\" value=\"$dept_code\">
-                                    <div class=\"text-secondary mt-2 mb-1\" style=\"font-size: 12px;\">Number of lines</div>
-                                    <div class=\"h5 font-weight-bold text-primary\" style=\"font-size: 50px; line-height: .85;\">$count</div>
-                                    <button type=\"submit\" name=\"submit\" class=\"btn btn-primary px-2 mt-n1\" style=\"border-radius: 8px; font-size: 12px; padding: 1px 1px;\">More Info</button>
-                                </form>
-                            </div>
-                            <div class=\"text-center ml-4 mt-3 pr-2 pt-2\">
-                                <div class=\"h6 mb-0 font-weight-bold text-gray-800\" style=\"font-size: 26px;\">$name</div>
-                                <div class=\"h6 text-muted\" style=\"font-size: 30px;\">$number</div>
-                            </div>
-                        </div>
+            <div class=\"col-lg-3 col-md-4 col-sm-8 mb-4\">
+              <div class=\"card shadow h-100\" style=\"border-radius: 8px; border-left: 5px solid #4e73df;\">
+                <div class=\"card-body\">
+                  <div class=\"row justify-content-center\" >
+                    <div class=\"text-center ml-2 mr-1\">
+                      <form action=\"index.php\" method=\"post\" class=\"d-flex flex-column align-items-center py-1\" style=\"gap: 5px; line-height: .75;\">
+                          <input type=\"hidden\" name=\"depart_code\" value=\"$dept_code\">
+                          <div class=\"text-secondary mt-2 mb-1\" style=\"font-size: 12px;\">Number of lines</div>
+                          <div class=\"h1 font-weight-bold text-primary\" style=\"line-height: .75;\">$count</div>
+                          <button type=\"submit\" name=\"submit\" class=\"btn btn-primary px-2 mt-n1\" style=\"border-radius: 8px; font-size: 12px; padding: 1px 1px;\">More Info</button>
+                      </form>
                     </div>
+                    
+                    <div class=\"text-center my-2 mx-2 pt-3\">
+                      <div class=\"h5 mb-0 font-weight-bold text-gray-800\">$name</div>
+                      <div class=\"h5 text-muted\">$number</div>
+                    </div>
+                  </div>
                 </div>
-            </div>
-
-
-        
+              </div>
+            </div>       
           `;
       
           document.querySelector(\"#dashboad_insert\").insertAdjacentHTML(\"beforeend\", table);
