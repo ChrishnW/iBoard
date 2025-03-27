c55ea4098440011758d5124da935e108db74c9d5
s3rb0g <raniegobres27@gmail.com>
2025-03-27 12:08:36 +0800

    adjust layout and font size in index.php

--- pages/index.php
+++ pages/index.php
@@ -196,14 +196,14 @@
                             <div class=\"text-center ml-2 mr-1\">
                                 <form action=\"index.php\" method=\"post\" class=\"d-flex flex-column align-items-center py-1\" style=\"gap: 5px; line-height: .85;\">
                                     <input type=\"hidden\" name=\"depart_code\" value=\"$dept_code\">
-                                    <div class=\"text-secondary mt-2\" style=\"font-size: 12px;\">number of lines</div>
+                                    <div class=\"text-secondary mt-2 mb-1\" style=\"font-size: 12px;\">Number of lines</div>
                                     <div class=\"h5 font-weight-bold text-primary\" style=\"font-size: 50px; line-height: .85;\">$count</div>
                                     <button type=\"submit\" name=\"submit\" class=\"btn btn-primary px-2 mt-n1\" style=\"border-radius: 8px; font-size: 12px; padding: 1px 1px;\">More Info</button>
                                 </form>
                             </div>
-                            <div class=\"text-center ml-3 mt-3 pr-2\">
+                            <div class=\"text-center ml-4 mt-3 pr-2\">
                                 <div class=\"h6 mb-0 font-weight-bold text-gray-800\" style=\"font-size: 26px;\">$name</div>
-                                <div class=\"h6 text-muted\" style=\"font-size: 26px;\">$number</div>
+                                <div class=\"h6 text-muted\" style=\"font-size: 30px;\">$number</div>
                             </div>
                         </div>
                     </div>
