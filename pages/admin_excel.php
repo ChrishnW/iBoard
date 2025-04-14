c0e1434667c38131007c1794d870ccf05bafa7d9
s3rb0g <raniegobres27@gmail.com>
2025-04-14 08:22:29 +0800

    Add building information to admin Excel export

--- pages/admin_excel.php
+++ pages/admin_excel.php
@@ -35,6 +35,7 @@
                 <tr style="background-color: #333; color: white; padding: 8px; text-align: center; font-weight: bold;">
                     <th>Date</th>
                     <th>Department</th>
+                    <th>Building</th>
                     <th>Model</th>
                     <th>Unit</th>
                     <th>Status</th>
@@ -59,7 +60,7 @@
                         $formattedDate = $startDate->format('Y-m-d');
 
                         $conn->next_result();
-                        $result = mysqli_query($conn,"SELECT tbl_records.date, tbl_department.dept_name, tbl_records.model,tbl_records.unit, tbl_records.status, tbl_records.target_day, tbl_records.actual, tbl_records.balance, tbl_line.incharge_name FROM tbl_records INNER JOIN tbl_line ON tbl_line.line_name=tbl_records.unit INNER JOIN tbl_accounts ON tbl_accounts.username=tbl_records.model INNER JOIN tbl_department ON tbl_department.dept_code=tbl_accounts.dept_code WHERE tbl_records.date='$formattedDate'");  
+                        $result = mysqli_query($conn,"SELECT tbl_line.building, tbl_records.date, tbl_department.dept_name, tbl_records.model,tbl_records.unit, tbl_records.status, tbl_records.target_day, tbl_records.actual, tbl_records.balance, tbl_line.incharge_name FROM tbl_records INNER JOIN tbl_line ON tbl_line.line_name=tbl_records.unit INNER JOIN tbl_accounts ON tbl_accounts.username=tbl_records.model INNER JOIN tbl_department ON tbl_department.dept_code=tbl_accounts.dept_code WHERE tbl_records.date='$formattedDate'");  
                         while($row = mysqli_fetch_array($result))
                         {
                             $j++;
@@ -67,6 +68,7 @@
                         <tr style='background-color: ". (($j % 2 == 0) ? "#d1e7dd" : "#f8f9fa") .";'>
                             <td style='text-align: left;'>". $row['date'] ."</td>
                             <td style='text-align: left;'>". $row['dept_name'] ."</td>
+                            <td style='text-align: left;'>". $row['building'] ."</td>
                             <td style='text-align: left;'>". $row['model'] ."</td>
                             <td style='text-align: left;'>". $row['unit'] ."</td>
                             <td style='text-align: left;'>". $row['status'] ."</td>
